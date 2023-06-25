d62f6e9fe06f4b9ffe7674c108763d70c17631f5
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2023-06-24 20:58:35 -0400

    refactor: Accuracy ram info

--- client/template/bot_info_template.go
+++ client/template/bot_info_template.go
@@ -21,12 +21,27 @@ var BotInfoTemplate = func(session *discordgo.Session, locale discordgo.Locale) 
 			embed.NewEmbedField(i18n.Translate("Guilds", locale), fmt.Sprintf("```%d```", len(session.State.Guilds))),
 			embed.NewEmbedField(i18n.Translate("Users", locale), fmt.Sprintf("```%d```", users)),
 			embed.NewEmbedField(i18n.Translate("CPUAmount", locale), fmt.Sprintf("```%s```", cpus)),
-			embed.NewEmbedField(i18n.Translate("RAMUsage", locale), fmt.Sprintf("```%s MB```", ram)),
+			embed.NewEmbedField(i18n.Translate("RAMUsage", locale), fmt.Sprintf("```%s```", ram)),
 		)
 }
 
 func getHostInfo() (cpus, ram string) {
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
-	return strconv.Itoa(runtime.NumCPU()), strconv.Itoa(int(m.Sys / 1024 / 1024))
+	return strconv.Itoa(runtime.NumCPU()), formatRam(m.Alloc)
+}
+
+// formatRam formats the ram usage in KB
+func formatRam(ram uint64) string {
+	const unit = 1000
+	if ram < unit {
+		return fmt.Sprintf("%d B", ram)
+	}
+	div, exp := int64(unit), 0
+	for n := ram / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB",
+		float64(ram)/float64(div), "kMGTPE"[exp])
 }
