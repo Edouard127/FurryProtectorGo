919006a45476b36f2e08f012ff721d2394459eea
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2023-06-26 14:24:06 -0400

    fix: Wrong index

--- core/data/permission.go
+++ core/data/permission.go
@@ -51,5 +51,25 @@ const (
 )
 
 func (u UserPermission) String() string {
-	return [...]string{"CreateInstantInvite", "KickMembers", "BanMembers", "Administrator", "ManageChannels", "ManageGuild", "AddReactions", "ViewAuditLog", "PrioritySpeaker", "Stream", "ViewChannel", "SendMessages", "SendTTSMessages", "ManageMessages", "EmbedLinks", "AttachFiles", "ReadMessageHistory", "MentionEveryone", "UseExternalEmojis", "ViewGuildInsights", "Connect", "Speak", "MuteMembers", "DeafenMembers", "MoveMembers", "UseVAD", "ChangeNickname", "ManageNicknames", "ManageRoles", "ManageWebhooks", "ManageGuildExpressions", "UseApplicationCommands", "RequestToSpeak", "ManageEvents", "ManageThreads", "CreatePublicThreads", "CreatePrivateThreads", "UseExternalStickers", "SendMessagesInThreads", "StartEmbeddedActivities", "ModerateMembers", "ViewCreatorMonetization", "UseSoundboard", "ExternalSounds", "SendVoiceMessages"}[u-1]
+	return [...]string{"CreateInstantInvite", "KickMembers", "BanMembers", "Administrator", "ManageChannels", "ManageGuild", "AddReactions", "ViewAuditLog", "PrioritySpeaker", "Stream", "ViewChannel", "SendMessages", "SendTTSMessages", "ManageMessages", "EmbedLinks", "AttachFiles", "ReadMessageHistory", "MentionEveryone", "UseExternalEmojis", "ViewGuildInsights", "Connect", "Speak", "MuteMembers", "DeafenMembers", "MoveMembers", "UseVAD", "ChangeNickname", "ManageNicknames", "ManageRoles", "ManageWebhooks", "ManageGuildExpressions", "UseApplicationCommands", "RequestToSpeak", "ManageEvents", "ManageThreads", "CreatePublicThreads", "CreatePrivateThreads", "UseExternalStickers", "SendMessagesInThreads", "StartEmbeddedActivities", "ModerateMembers", "ViewCreatorMonetization", "UseSoundboard", "ExternalSounds", "SendVoiceMessages"}[getIndex(u)]
+}
+
+func getIndex(n UserPermission) int {
+	low := 0
+	high := 44
+
+	for low < high {
+		mid := (low + high) / 2
+
+		switch {
+		case n < 1<<uint(mid):
+			high = mid
+		case n > 1<<uint(mid):
+			low = mid + 1
+		default:
+			return mid
+		}
+	}
+
+	return 0
 }
